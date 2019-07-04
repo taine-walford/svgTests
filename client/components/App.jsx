443a3ef7cf09031537fd551a4104784a6ec4de39
Taine Walford <tainewalford@live.com>
2019-07-05 11:52:49 +1200

    Added darkenCOlour function

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,8 +6,27 @@ import Pants from './Pants'
 import Shoes from'./Shoes'
 
 const App = () => {
+  const colour = '#7D7D7D'
+
+  const darkenColour = (colour) => {
+    return () => {
+      const bitArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
+      console.log("Base colour:", colour)
+      let tempColour = [colour.slice(1,3), colour.slice(3,5), colour.slice(5, 7)]
+      tempColour =tempColour.map(bit => {
+        let bitIndex = bitArray.indexOf(bit[0]) 
+        bitIndex = (bitIndex === 0) ? bitIndex: bitIndex - 1
+        return (bitArray[bitIndex] + bit[1])
+      })
+      console.log(tempColour)
+      tempColour = '#' + tempColour[0] + tempColour[1] + tempColour[2]
+      console.log("New colour:", tempColour)
+    }
+  }
+
   return <>
     <h1>React development has begun!</h1>
+    <button onClick={darkenColour(colour)}>Darken colour</button>
     <div className='svgContainer'>
       <Shirt />
       <Tie />
