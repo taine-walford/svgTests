f9dfade530cfbcdbbec54c6248e5ec0ae4d82ad3
Taine Walford <tainewalford@live.com>
2019-07-04 23:59:12 +1200

    got some components working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Jacket from './Jacket';
+import Shirt from './Shirt'
+import Tie from './Tie'
 
 const App = () => {
-  return (
+  return <>
     <h1>React development has begun!</h1>
-  )
+    <div className='svgContainer'>
+      <Shirt />
+      <Tie />
+      <Jacket />
+    </div>
+  </>
 }
 
 export default App
---
+++ client/components/Tie.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Tie({ colour }) {
+  return <>
+    <svg width="564" height="564" version="1.1" viewBox="0 0 564 564" xmlns="http://www.w3.org/2000/svg">
+      <g>
+        <g fill="#2d2d2d" stroke="#000" strokeWidth="1px">
+          <path d="m279.5 38.167 6.8333 0.16667 3.6667 5.8333-2.1667 8 7.1667 43.667-11.167 50.5-12-51.5 7.3333-42.5-3.3333-7.5z" />
+          <path d="m279.17 52.333 8.6667-0.16667z" />
+          <path d="m279.17 52.333 8.6667-0.16667z" />
+        </g>
+      </g>
+    </svg>
+  </>
+}
